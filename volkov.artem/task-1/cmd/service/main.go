c7efa295731707afb51d5211e22558cd3443a917
Wolfmano <volkov_av07@mail.ru>
2026-09-19 11:06:47 +0300

    [TASK-1] Task Well Done

--- volkov.artem/task-1/cmd/service/main.go
+++ volkov.artem/task-1/cmd/service/main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var firstInt int
+	var secondInt int
+	var operand string
+	_, err := fmt.Scan(&firstInt)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&secondInt)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	fmt.Scan(&operand)
+	switch operand {
+	case "+":
+		fmt.Println(firstInt + secondInt)
+	case "-":
+		fmt.Println(firstInt - secondInt)
+	case "*":
+		fmt.Println(firstInt * secondInt)
+	case "/":
+		if secondInt == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(firstInt / secondInt)
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
 }
